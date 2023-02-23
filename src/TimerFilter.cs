d0ba76d97b60c4ad46fc28a497c8a3a69224a780
Adam Gardner <adga2012@gmail.com>
2023-02-23 15:42:04 -0700

    initial commit

---
+++ src/TimerFilter.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Logging;
+
+namespace TimerFilter;
+
+public class TimerFilter : IAsyncActionFilter
+{
+    private readonly ILogger<TimerFilter> _logger;
+
+    public TimerFilter(ILogger<TimerFilter> logger)
+    {
+        _logger = logger;
+    }
+
+    public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+    {
+        var sw = Stopwatch.StartNew();
+
+        try
+        {
+            await next();
+        }
+        finally
+        {
+            sw.Stop();
+            var actionName = context.ActionDescriptor.DisplayName;
+            _logger.LogInformation("{0} took {1}ms", actionName, sw.ElapsedMilliseconds.ToString());
+        }
+    }
+}
